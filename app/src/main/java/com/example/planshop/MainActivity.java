f7d61a9242d7979d28b5ec74d62a79173c4485ee
michalkin9 <michalkin9@gmail.com>
2019-11-21 13:10:40 +0200

    login and sign up act

--- app/src/main/java/com/example/planshop/MainActivity.java
+++ app/src/main/java/com/example/planshop/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.planshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signUp(View view){
+        Intent sign = new Intent (this, signUp.class);
+        startActivity(sign);
     }
 }
