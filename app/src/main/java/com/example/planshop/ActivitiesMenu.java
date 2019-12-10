2770e0a2b5e0ac706fd0d0e7f1026c24b4afafa0
yael hava <yaelhava2610@gmail.com>
2019-12-10 19:58:37 +0200

    add activities

---
+++ app/src/main/java/com/example/planshop/ActivitiesMenu.java
@@ -0,0 +1,14 @@
+package com.example.planshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitiesMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities_menu);
+    }
+}
