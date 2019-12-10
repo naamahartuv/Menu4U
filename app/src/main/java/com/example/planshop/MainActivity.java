3917d57af1ddf168dd5f5986b3e610f582396269
naamahartuv <naama.hartuv@gmail.com>
2019-12-10 13:30:56 +0200

    update

--- app/src/main/java/com/example/planshop/MainActivity.java
+++ app/src/main/java/com/example/planshop/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.planshop;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/planshop/signUp.java
+++ app/src/main/java/com/example/planshop/signUp.java
@@ -1,13 +1,23 @@
 package com.example.planshop;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import com.firebase.client.Firebase;
+
 public class signUp extends AppCompatActivity {
+
+//    private Firebase users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+//        Firebase.setAndroidContext(this);
+//        users = new Firebase("https://plan-shop.firebaseio.com/");
+
     }
 }
