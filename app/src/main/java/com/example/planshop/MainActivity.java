4305b394a5642d3ee4f14eb572660c5a5c15229b
yael hava <yaelhava2610@gmail.com>
2019-12-14 23:36:21 +0200

    login

--- app/src/main/java/com/example/planshop/MainActivity.java
+++ app/src/main/java/com/example/planshop/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.planshop;
 import android.content.Intent;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,15 +18,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText txtEmail, txtPassword;
-    private FirebaseDatabase mDatabase;
+    private FirebaseAuth mAuth;
 
-
+//    private FirebaseDatabase mDatabase;
 
 
     @Override
@@ -31,14 +36,81 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void signUp(View view){
-        Intent sign = new Intent (this, signUp.class);
+    public void signUp(View view) {
+        Intent sign = new Intent(this, signUp.class);
         startActivity(sign);
     }
 
-    public void logIn(View view){
+    public void logIn(View view) {
+        mAuth = FirebaseAuth.getInstance();
+
+        // initialising all views through id defined above
         txtEmail = findViewById(R.id.editMainEmail);
         txtPassword = findViewById(R.id.editMainPass);
+
+        String email, password;
+        email = txtEmail.getText().toString();
+        password = txtPassword.getText().toString();
+
+        // validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter email!!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter password!!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // signin existing user
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(
+                                    @NonNull Task<AuthResult> task)
+                            {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(),
+                                            "Login successful!!",
+                                            Toast.LENGTH_LONG)
+                                            .show();
+
+                                    // if sign-in is successful
+                                    Intent intent = new Intent(MainActivity.this, ActivitiesMenu.class);
+                                    startActivity(intent);
+                                }
+
+                                else {
+
+                                    // sign-in failed
+                                    Toast.makeText(getApplicationContext(),
+                                            "Login failed!!",
+                                            Toast.LENGTH_LONG)
+                                            .show();
+
+                                }
+                            }
+                        });
+
+
+    }
+
+
+
+
+
+
+}
+
+
 
 //        mDatabase = FirebaseDatabase.getInstance();
 //
@@ -63,9 +135,3 @@ public class MainActivity extends AppCompatActivity {
 //            public void onCancelled(DatabaseError databaseError) {
 //            }
 //        });
-
-        Intent log = new Intent (this, ActivitiesMenu.class);
-        startActivity(log);
-    }
-
-}
