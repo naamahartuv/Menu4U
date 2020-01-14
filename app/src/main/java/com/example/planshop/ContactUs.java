ba1d23e11d58941774c3a8d061ebb76cf3aa62d9
naamahartuv <naama.hartuv@gmail.com>
2020-01-14 16:29:02 +0200

    phone call

--- app/src/main/java/com/example/planshop/ContactUs.java
+++ app/src/main/java/com/example/planshop/ContactUs.java
@@ -1,19 +1,65 @@
 package com.example.planshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ContactUs extends AppCompatActivity {
+    private static final int REQUEST_CALL = 1;
+//    private TextView phonNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+//        phonNumber = findViewById(R.id.phonNumber);
+        ImageView imageCall = findViewById(R.id.image_call);
+
+        imageCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makePhoneCall();
+            }
+        });
+    }
+
+    private void makePhoneCall(){
+        String number = "0544323897";
+        if (ContextCompat.checkSelfPermission(ContactUs.this,
+                Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(ContactUs.this,
+                    new String[] {Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        } else{
+            String dial = "tel:" + number;
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CALL){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                makePhoneCall();
+            }else{
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
